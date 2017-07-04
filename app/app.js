7625cd26ab0a0746c3404f86b772ef544fd88a18
niksmanov <niks.manov@gmail.com>
2017-07-04 14:52:18 +0300

    add test record to database

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const data = require('./data');
 
 require('./config/app.config')(app);
 require('./config/auth.config')(app);
--- app/data/pets-data.js
+++ app/data/pets-data.js
@@ -2,3 +2,17 @@ const { MongoClient } = require('mongodb');
 
 const connectionString = 'mongodb://localhost/PetVetDb';
 const dbConnection = MongoClient.connect(connectionString);
+
+dbConnection
+    .then((connection) => {
+        connection.collection('Pets').insertOne({
+            name: 'Lucky',
+            type: 'dog',
+            breed: 'Husky',
+            age: '3',
+            checkUp: '23.06.2017',
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
