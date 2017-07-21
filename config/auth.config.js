ebdb48ac7c4379b38faac72a672d02692d267dde
vasilvalkov <vasil.valkov@gmail.com>
2017-07-21 23:20:03 +0300

    Implemented Storing session. Now closing the tab or browser and reopening the page doesn't logout the user.

--- config/auth.config.js
+++ config/auth.config.js
@@ -4,13 +4,20 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 // const mongoDb = require('mongodb').MongoClient;
-// const MongoStore = require('connect-mongo')(session);
-// const config = require('./app.config');
+const MongoStore = require('connect-mongo')(session);
+const config = require('./app.config');
 const passportConfig = require('./passport.config');
 
 const init = (app, data) => {
     app.use(cookieParser());
-    app.use(session({ secret: 'Pet Vet' }));
+    app.use(session({
+        secret: 'Pet Vet',
+        store: new MongoStore({
+            url: config.connectionString,
+            autoRemove: 'native', // Default
+            }),
+        }));
+    // app.use(session({ secret: 'Pet Vet' }));
     app.use(passport.initialize());
     app.use(passport.session());
 
